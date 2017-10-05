9c33f2730b7804522abf26fb536662bef985f24a
Andy Dayton <amdayton@gmail.com>
2017-10-05 10:20:13 -0500

    Updated to use topojson

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 import styles from './styles.scss';
 import * as Leaflet from 'leaflet';
+import * as topojson from 'topojson';
 import 'whatwg-fetch';
 
-const osmUrl = 'http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.png';
+const tilesUrl = 'http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.png';
 
 const featureStyles = {
   fill: false,
   weight: 2
 }
 
+// create map container element
 let mapContainer = document.createElement('div');
 mapContainer.id = 'map-container';
 document.body.appendChild(mapContainer);
 
-const map = Leaflet.map('map-container').setView([47.47184, -92.96549], 10);
-let osm = new Leaflet.TileLayer(osmUrl);
+// create loading indicator
+let loadingContainer = document.createElement('div');
+loadingContainer.id = 'loading';
+loadingContainer.innerHTML = 'Loading watershed boundaries...';
+document.body.appendChild(loadingContainer); 
 
-map.addLayer(osm);	
+// set up map and map tile layer
+const map = Leaflet.map('map-container')
+  .setView([47.47184, -92.96549], 12);
+const tiles = new Leaflet.TileLayer(tilesUrl);
 
-fetch('data/watersheds.geojson')
+// add map tile layer
+map.addLayer(tiles);	
+
+// load topojson and add tp map
+fetch('data/watersheds.topo.json')
   .then((res) => res.json())
   .then((json) => {
-    console.log(json);
-    Leaflet.geoJSON(json, {
+  
+    // convert topojson to geojson
+    let geojson = topojson.feature(json, json.objects.watersheds);
+
+    Leaflet.geoJSON(geojson, {
       style: (feature) => featureStyles
     }).addTo(map)
+
+    loadingContainer.className = 'done';
   });
